9ac2510f5834368000a73bb81ce038c82ae5b077
Silvestre Fernandez <rasa356@outlook.com>
2022-09-12 13:32:12 +1200

    updating my work so far

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-/* When the user scrolls the page, execute myFunction */
-window.onscroll = function() {myFunction()};
+let navbarElement = document.getElementById('navbar');
+// let currentColourIndex = 0;
+// let buttons = document.getElementsByClassName('buttons');
 
-// Get the navbar 
-let navbar = document.getElementsByClassName("navbar");
-
-//  Get the offset position of the navbar
-let sticky = navbar.offsetTop;
-
-/*  Add the sticky variable to the navbar when you reach the position of scrolling. Remove the 
-sticky variable when you leave the scroll position */
-
-function myFunction() {
-    if (window.scrollY) {
-        navbar.classList.add(sticky);
+const themeButton = document.getElementById('changeThemeButton');
+const bodyStyle = document.getElementById('body');
+function changeColour() {
+    if (bodyStyle.style.background === 'white') {
+        bodyStyle.style.background = 'gray';
     } else {
-        navbar.classList.remove(sticky);
+        bodyStyle.style.background = 'white';
     }
 }
+
+let themeBtn = themeButton.addEventListener('click', (event) => {
+    console.log(`button has been clicked`);
+    changeColour();
+    changeBodyTextColour();
+})
+
+function changeBodyTextColour() {
+    if (bodyStyle.style.color === 'black') {
+        bodyStyle.style.color = 'blanchedalmond';
+    } else {
+        bodyStyle.style.color = 'black';
+    }
+}
+
+function changeThemeText() {
+    if (document.getElementById('theme-txt').innerText === 'Light Theme') {
+        document.getElementById('theme-txt').innerText = 'Light Theme';
+    } else {
+        document.getElementById('theme-txt').innerText = 'Dark Theme';
+    }
+}
+
+
